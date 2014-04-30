273faa646d5aef61d5d2a432f5dae2a8d789c2da
Matt <mattmsj24@gmail.com>
2014-04-29 22:11:31 -0600

    lab One Done

--- GameSkeleton/GameSolution/Engine/Vector2.h
+++ GameSkeleton/GameSolution/Engine/Vector2.h
@@ -1,19 +1,28 @@
-#ifndef VECTOR_2
-#define VECTOR_2;
-#endif // !VECTOR2
-
 #include<iostream>
-
+#include<cmath>
+#ifndef VECTOR_2
+#define VECTOR_2
 
 
 struct Vector2
 {
 	float x;
 	float y;
-	
+
 	Vector2(float x = 0, float y = 0) : x(x), y(y) {}
-	
+
+	operator float*(){ return &x; }
+
+	float dotReturn;
+	float Dot(const Vector2& a, const Vector2& b)
+	{
+		return ((a.x)*(b.x)) + ((a.y)*(b.y));
+
+	};
+
 };
+
+
 
 Vector2 operator+(const Vector2& left, const Vector2& right)
 {
@@ -24,6 +33,7 @@ Vector2 operator+(const Vector2& left, const Vector2& right)
 	return returnVector;
 
 }
+
 
 Vector2 operator-(const Vector2& left, const Vector2& right)
 {
@@ -52,9 +62,79 @@ Vector2 operator*(const Vector2& left, const float& right)
 	return returnVector;
 }
 
+Vector2 operator/(const Vector2& left, const float& right)
+{
+	Vector2 returnVector;
+	returnVector.x = left.x / right;
+	returnVector.y = left.y / right;
+
+	return returnVector;
+}
+
+float Length(const Vector2& data)
+{
+	return sqrt(pow(data.x, 2) + pow(data.y, 2));
+}
+
+float LengthSquared(const Vector2& data)
+{
+	return pow(Length(data),2);
+}
+
+
+Vector2 normalized(const Vector2& preNormalizedVector)
+{
+	float length = Length(preNormalizedVector);
+
+	Vector2 normVector;
+	normVector = preNormalizedVector / length;
+	return normVector;
+}
 
 std::ostream& operator<<(std::ostream stream, const Vector2& right)
 {
 	std::cout << "{" << right.x << ", " << right.y << "}";
 	return stream;
 }
+
+Vector2 cwPerpVector;
+Vector2 cwNewData;
+Vector2 PerpCW(const Vector2& data)
+{
+	if (data.x == 0 && data.y == 0)
+	{
+		cwNewData = Vector2(.0000001, .0000001);
+		cwPerpVector = Vector2(-(cwNewData.y), cwNewData.x);
+	}
+	else{
+		cwPerpVector = Vector2(-(data.y), data.x);
+	}
+	return cwPerpVector;
+}
+
+Vector2 ccwPerpVector;
+Vector2 ccwNewData;
+
+Vector2 PerpCCW(const Vector2& data)
+{
+	if (data.x == 0 && data.y == 0)
+	{
+		ccwNewData = Vector2(.0000001, .0000001);
+		ccwPerpVector = Vector2(ccwNewData.x, -(ccwNewData.y));
+
+	}
+	else{
+		ccwPerpVector = Vector2(data.x, -(data.y));
+	}
+	return ccwPerpVector;
+
+}
+
+float dotReturn;
+float Dot(const Vector2& a, const Vector2& b)
+{
+	return ((a.x)*(b.x)) + ((a.y)*(b.y));
+	
+};
+
+#endif // !VECTOR2
