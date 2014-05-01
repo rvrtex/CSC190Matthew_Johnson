7bcee929d790147086d1a881d8bf3f2764384cb6
Matt <mattmsj24@gmail.com>
2014-05-01 09:11:35 -0600

    this ran fine

--- GameSkeleton/GameSolution/Game/main.cpp
+++ GameSkeleton/GameSolution/Game/main.cpp
@@ -1,7 +1,40 @@
 #include "Engine.h"
+#include"Core.h"
 
-int main()
+using Core::Input;
+
+const int SCREEN_WIDTH = 1024;
+const int SCREEN_HEIGHT = 728;
+
+bool red = true;
+
+bool Update(float dt)
 {
+
+	dt;	
+	if (Input::IsPressed(Input::KEY_ESCAPE))
+		return true;
+	return false;
+}
+
+void Draw(Core::Graphics& graphics)
+{
+	graphics.SetColor(RGB(255, 255, 255));
+	graphics.DrawString(SCREEN_WIDTH / 2 - 50, SCREEN_HEIGHT / 2 - 20, "Hello World");
+	graphics.DrawLine(400, 300, 10, 10);
+}
+
+void Flash(Core::Graphics& graphics)
+{
+	
+}
+void main()
+{
+	Core::Init("Matt's AWESOME Game Demo", SCREEN_WIDTH, SCREEN_HEIGHT);
+	Core::RegisterUpdateFn(Update);
+	Core::RegisterDrawFn(Draw);
+	Core::GameLoop();
+
 	Engine::sampleFunctionThatReturnsTrue();
 }
 
